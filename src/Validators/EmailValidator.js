0f18a0d33e55f922d9b1f350d93bbd6a4b0aee67
Zarko <stojanov1987@gmail.com>
2024-07-05 22:24:54 +0200

    * added Validators +

--- src/Validators/EmailValidator.js
+++ src/Validators/EmailValidator.js
@@ -1,9 +1,20 @@
 
+const invalidEmails = [
+    'admin@gmail.com', 'test@gmail.com'
+]
 
 export  const EmailValidator = {
-    required: true,
+    required: 'Email je obavezan',
     pattern: {
-        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+        value: /^[A-Z0-9._%+-]+@gmail,com$/i,
         message: "invalid email address"
+    },
+    minLength: {
+      value:10,
+      message: "Email mora biti minimum 10 znakova"
+    },
+    validate: {
+        invalidEmailCheck: value => invalidEmails.includes(value) ? 'Ovaj email nije dozvoljen' : ''
     }
+
 }
--- src/Validators/PasswordValidator.js
+++ src/Validators/PasswordValidator.js
@@ -4,5 +4,13 @@ export const PasswordValidator = {
     required: true,
     validate: {
         trimCheck: value => value.trim() !== '' || 'Lozinka ne moze biti prazan prostor'
+    },
+    minLength: {
+        value: 5,
+        message: "Lozinka mora biti minimum 5 znakova"
+    },
+    maxLength: {
+        value: 16,
+        message: "Lozinka mora biti najvise 16 znakova"
     }
 }
